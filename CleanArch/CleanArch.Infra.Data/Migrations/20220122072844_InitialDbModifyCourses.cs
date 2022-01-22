4b5d49e781c317669e9cb6dc2e60f5f676a169ba
1089 <1089@PC434>
2022-01-22 11:01:41 +0330

    Initial UniversityDB

---
+++ CleanArch/CleanArch.Infra.Data/Migrations/20220122072844_InitialDbModifyCourses.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArch.Infra.Data.Migrations
+{
+    public partial class InitialDbModifyCourses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Type",
+                table: "Type");
+
+            migrationBuilder.RenameTable(
+                name: "Type",
+                newName: "Courses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Courses",
+                table: "Courses");
+
+            migrationBuilder.RenameTable(
+                name: "Courses",
+                newName: "Type");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Type",
+                table: "Type",
+                column: "Id");
+        }
+    }
+}
